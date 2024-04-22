df3c81046ceb7594b5f7f52b698338aaa2ca7990
Rafael FERNANDO <rafael.fernando@jobteaser.com>
2024-04-22 17:25:41 +0200

    revamp fonction création OK

--- Projet/bdd.sql
+++ Projet/bdd.sql
@@ -1,11 +1,31 @@
 CREATE DATABASE IF NOT EXISTS projetGolang;
 use projetGolang;
 
-CREATE TABLE IF NOT EXISTS events (
+CREATE TABLE IF NOT EXISTS salles (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    title TEXT NOT NULL,
-    date TIMESTAMP NOT NULL,
-    location TEXT NOT NULL,
-    category TEXT NOT NULL,
-    description TEXT NOT NULL
+    name TEXT NOT NULL,
+    capacity INT NOT NULL,
+    dispo INT NOT NULL
 );
+
+CREATE TABLE IF NOT EXISTS reservations (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name TEXT NOT NULL,
+    date TIMESTAMP NOT NULL,
+    cours TEXT NOT NULL,
+    description TEXT NOT NULL,
+    salle_id INT NOT NULL,
+    FOREIGN KEY (salle_id) REFERENCES salles(id)
+);
+
+INSERT INTO salles (name, capacity, dispo) VALUES 
+('Salle 1', 10, 1),
+('Salle 2', 25, 1),
+('Salle 3', 10, 1),
+('Salle 4', 15, 1),
+('Salle 5', 18, 1),
+('Salle 6', 50, 1),
+('Salle 7', 11, 1),
+('Salle 8', 20, 1),
+('Salle 9', 10, 1),
+('Salle 10', 25, 1);
--- Projet/fonction/fonction.go
+++ Projet/fonction/fonction.go
@@ -14,8 +14,14 @@ const (
 	dbName     = "projetGolang"
 )
 
-type Event struct {
-	ID          int
+type Room struct {
+	ID       int
+	Name     string
+	Capacity int
+	Dispo    int
+}
+
+type Reservation struct {
 	Title       string
 	Date        string
 	Location    string
@@ -41,15 +47,15 @@ func ConnexionBdd() (*sql.DB, error) {
 	return db, nil
 }
 
-func AddEvent(db *sql.DB, event Event) error {
-	query := "INSERT INTO events (title, date, location, category, description) VALUES (?, ?, ?, ?, ?)"
+func AddEvent(db *sql.DB, Reservation Reservation) error {
+	query := "INSERT INTO reservations (name, date, salle_id, cours, description) VALUES (?, ?, ?, ?, ?)"
 	stmt, err := db.Prepare(query)
 	if err != nil {
 		return err
 	}
 	defer stmt.Close()
 
-	_, err = stmt.Exec(event.Title, event.Date, event.Location, event.Category, event.Description)
+	_, err = stmt.Exec(Reservation.Title, Reservation.Date, Reservation.Location, Reservation.Category, Reservation.Description)
 	if err != nil {
 		return err
 	}
@@ -57,12 +63,12 @@ func AddEvent(db *sql.DB, event Event) error {
 	return nil
 }
 
-func CreationEvenement(db *sql.DB) error {
+func CreationReservations(db *sql.DB) error {
 	fmt.Println("----------------------------------------Créer son évènement---------------------------------------")
 
 	var title, date, timeInput, location, category, description string
 
-	fmt.Print("Entrez le titre de l'événement: ")
+	fmt.Print("Entrez le titre de la réservation: ")
 	fmt.Scanln(&title)
 
 	fmt.Print("Entrez la date (YYYY-MM-DD): ")
@@ -77,17 +83,21 @@ func CreationEvenement(db *sql.DB) error {
 	layout := "2006-01-02 15:04"
 	thenafter, err := time.Parse(layout, dateTimeInput)
 	if err != nil {
-		return fmt.Errorf("Erreur lors de l'analyse du temps: %v", err)
+		return fmt.Errorf("Erreur lors de l'analyse du temps (Veuillez entrer une date dans le format 'YYYY-MM-DD' ): %v", err)
+
 	}
 
-	fmt.Print("Entrez le lieu: ")
+	fmt.Print("Entrez la salle que vous souhaiter réserver (1-10): ")
 	fmt.Scanln(&location)
+	if location != "1" && location != "2" && location != "3" && location != "4" && location != "5" && location != "6" && location != "7" && location != "8" && location != "9" && location != "10" {
+		return fmt.Errorf("Salle invalide.")
+	}
 
 	for {
-		fmt.Print("Choisissez une catégorie (Professionnel, Personnel, Loisir): ")
+		fmt.Print("Choisissez la matière enseignée (LangageC, Go, Python): ")
 		fmt.Scanln(&category)
 
-		if category == "Professionnel" || category == "Personnel" || category == "Loisir" {
+		if category == "LangageC" || category == "Go" || category == "Python" {
 			break
 		}
 
@@ -99,7 +109,7 @@ func CreationEvenement(db *sql.DB) error {
 
 	fmt.Println("--------------------------------------------------------------------------------------------------")
 
-	newEvent := Event{
+	newReservation := Reservation{
 		Title:       title,
 		Date:        thenafter.Format("2006-01-02 15:04"),
 		Location:    location,
@@ -107,7 +117,7 @@ func CreationEvenement(db *sql.DB) error {
 		Description: description,
 	}
 
-	err = AddEvent(db, newEvent)
+	err = AddEvent(db, newReservation)
 	if err != nil {
 		return fmt.Errorf("Erreur lors de l'ajout de l'événement: %v", err)
 	}
@@ -116,66 +126,66 @@ func CreationEvenement(db *sql.DB) error {
 	return nil
 }
 
-func VisualiserEvenements(db *sql.DB) error {
-	fmt.Println("--------------------------------------Visualiser les événements--------------------------------------")
+// func VisualiserEvenements(db *sql.DB) error {
+// 	fmt.Println("--------------------------------------Visualiser les événements--------------------------------------")
 
-	rows, err := db.Query("SELECT * FROM events")
-	if err != nil {
-		return fmt.Errorf("Erreur lors de la récupération des événements: %v", err)
-	}
-	defer rows.Close()
+// 	rows, err := db.Query("SELECT * FROM events")
+// 	if err != nil {
+// 		return fmt.Errorf("Erreur lors de la récupération des événements: %v", err)
+// 	}
+// 	defer rows.Close()
 
-	fmt.Printf("%-5s | %-20s | %-19s | %-30s | %-15s | %-50s\n", "ID", "Titre", "Date", "Lieu", "Catégorie", "Description")
-	fmt.Println("--------------------------------------------------------------------------------------------------")
+// 	fmt.Printf("%-5s | %-20s | %-19s | %-30s | %-15s | %-50s\n", "ID", "Titre", "Date", "Lieu", "Catégorie", "Description")
+// 	fmt.Println("--------------------------------------------------------------------------------------------------")
 
-	for rows.Next() {
-		var event Event
-		if err := rows.Scan(&event.ID, &event.Title, &event.Date, &event.Location, &event.Category, &event.Description); err != nil {
-			return fmt.Errorf("Erreur lors de la lecture de la ligne d'événement: %v", err)
-		}
+// 	for rows.Next() {
+// 		var event Event
+// 		if err := rows.Scan(&event.ID, &event.Title, &event.Date, &event.Location, &event.Category, &event.Description); err != nil {
+// 			return fmt.Errorf("Erreur lors de la lecture de la ligne d'événement: %v", err)
+// 		}
 
-		fmt.Printf("%-5d | %-20s | %-19s | %-30s | %-15s | %-50s\n", event.ID, event.Title, event.Date, event.Location, event.Category, event.Description)
-	}
+// 		fmt.Printf("%-5d | %-20s | %-19s | %-30s | %-15s | %-50s\n", event.ID, event.Title, event.Date, event.Location, event.Category, event.Description)
+// 	}
 
-	fmt.Println("--------------------------------------------------------------------------------------------------")
+// 	fmt.Println("--------------------------------------------------------------------------------------------------")
 
-	return nil
-}
+// 	return nil
+// }
 
-func SupprimerEvenement(db *sql.DB, eventID int) error {
-	result, err := db.Exec("DELETE FROM events WHERE id = ?", eventID)
-	if err != nil {
-		return fmt.Errorf("Erreur lors de la suppression de l'événement: %v", err)
-	}
+// func SupprimerEvenement(db *sql.DB, eventID int) error {
+// 	result, err := db.Exec("DELETE FROM events WHERE id = ?", eventID)
+// 	if err != nil {
+// 		return fmt.Errorf("Erreur lors de la suppression de l'événement: %v", err)
+// 	}
 
-	rowsAffected, err := result.RowsAffected()
-	if err != nil {
-		return fmt.Errorf("Erreur lors de la récupération du nombre de lignes affectées: %v", err)
-	}
+// 	rowsAffected, err := result.RowsAffected()
+// 	if err != nil {
+// 		return fmt.Errorf("Erreur lors de la récupération du nombre de lignes affectées: %v", err)
+// 	}
 
-	if rowsAffected == 0 {
-		return fmt.Errorf("Aucun événement trouvé avec l'ID %d", eventID)
-	}
+// 	if rowsAffected == 0 {
+// 		return fmt.Errorf("Aucun événement trouvé avec l'ID %d", eventID)
+// 	}
 
-	fmt.Printf("Événement avec l'ID %d supprimé avec succès.\n", eventID)
+// 	fmt.Printf("Événement avec l'ID %d supprimé avec succès.\n", eventID)
 
-	return nil
-}
+// 	return nil
+// }
 
-func AfficherEvenement(db *sql.DB, eventID int) error {
-	fmt.Println("--------------------------------------------------------------------------------------------------")
-	row := db.QueryRow("SELECT * FROM events WHERE id = ?", eventID)
+// func AfficherEvenement(db *sql.DB, eventID int) error {
+// 	fmt.Println("--------------------------------------------------------------------------------------------------")
+// 	row := db.QueryRow("SELECT * FROM events WHERE id = ?", eventID)
 
-	var event Event
-	err := row.Scan(&event.ID, &event.Title, &event.Date, &event.Location, &event.Category, &event.Description)
-	if err != nil {
-		return fmt.Errorf("Erreur lors de la récupération de l'événement: %v", err)
-	}
+// 	var event Event
+// 	err := row.Scan(&event.ID, &event.Title, &event.Date, &event.Location, &event.Category, &event.Description)
+// 	if err != nil {
+// 		return fmt.Errorf("Erreur lors de la récupération de l'événement: %v", err)
+// 	}
 
-	fmt.Printf("ID: %d\nTitre: %s\nDate: %s\nLieu: %s\nCatégorie: %s\nDescription: %s\n",
-		event.ID, event.Title, event.Date, event.Location, event.Category, event.Description)
+// 	fmt.Printf("ID: %d\nTitre: %s\nDate: %s\nLieu: %s\nCatégorie: %s\nDescription: %s\n",
+// 		event.ID, event.Title, event.Date, event.Location, event.Category, event.Description)
 
-	fmt.Println("--------------------------------------------------------------------------------------------------")
+// 	fmt.Println("--------------------------------------------------------------------------------------------------")
 
-	return nil
-}
+// 	return nil
+// }
