1951dda154c8bbd6c060322269dc18b0ec427a76
Rafjt <118538315+Rafjt@users.noreply.github.com>
2024-04-23 09:41:26 +0200

    revamp fonction création OK (#1)
    * revamp fonction création OK
    
    * reste des fonctions
    
    * ++

--- Projet/fonction/fonction.go
+++ Projet/fonction/fonction.go
@@ -1,8 +1,12 @@
 package fonction
 
 import (
+	"bufio"
 	"database/sql"
 	"fmt"
+	"os"
+	"strconv"
+	"strings"
 	"time"
 
 	_ "github.com/go-sql-driver/mysql"
@@ -14,8 +18,14 @@ const (
 	dbName     = "projetGolang"
 )
 
-type Event struct {
-	ID          int
+type Room struct {
+	ID       int
+	Name     string
+	Capacity int
+	Dispo    int
+}
+
+type Reservation struct {
 	Title       string
 	Date        string
 	Location    string
@@ -41,15 +51,24 @@ func ConnexionBdd() (*sql.DB, error) {
 	return db, nil
 }
 
-func AddEvent(db *sql.DB, event Event) error {
-	query := "INSERT INTO events (title, date, location, category, description) VALUES (?, ?, ?, ?, ?)"
+func MakeSalleIndispobible(db *sql.DB, salleID int) error {
+	_, err := db.Exec("UPDATE salles SET dispo = 0 WHERE id = ?", salleID)
+	if err != nil {
+		return fmt.Errorf("Erreur lors de la mise à jour de la salle: %v", err)
+	}
+
+	return nil
+}
+
+func AddEvent(db *sql.DB, Reservation Reservation) error {
+	query := "INSERT INTO reservations (name, date, salle_id, cours, description) VALUES (?, ?, ?, ?, ?)"
 	stmt, err := db.Prepare(query)
 	if err != nil {
 		return err
 	}
 	defer stmt.Close()
 
-	_, err = stmt.Exec(event.Title, event.Date, event.Location, event.Category, event.Description)
+	_, err = stmt.Exec(Reservation.Title, Reservation.Date, Reservation.Location, Reservation.Category, Reservation.Description)
 	if err != nil {
 		return err
 	}
@@ -57,19 +76,24 @@ func AddEvent(db *sql.DB, event Event) error {
 	return nil
 }
 
-func CreationEvenement(db *sql.DB) error {
+func CreationReservations(db *sql.DB) error {
 	fmt.Println("----------------------------------------Créer son évènement---------------------------------------")
 
 	var title, date, timeInput, location, category, description string
 
-	fmt.Print("Entrez le titre de l'événement: ")
-	fmt.Scanln(&title)
+	reader := bufio.NewReader(os.Stdin)
+
+	fmt.Print("Entrez le titre de la réservation: ")
+	title, _ = reader.ReadString('\n')
+	title = strings.TrimSpace(title)
 
 	fmt.Print("Entrez la date (YYYY-MM-DD): ")
-	fmt.Scanln(&date)
+	date, _ = reader.ReadString('\n')
+	date = strings.TrimSpace(date)
 
 	fmt.Print("Entrez l'heure (HH:mm): ")
-	fmt.Scanln(&timeInput)
+	timeInput, _ = reader.ReadString('\n')
+	timeInput = strings.TrimSpace(timeInput)
 
 	// pour éviter l'erreur de temps/date
 	dateTimeInput := fmt.Sprintf("%s %s", date, timeInput)
@@ -77,17 +101,21 @@ func CreationEvenement(db *sql.DB) error {
 	layout := "2006-01-02 15:04"
 	thenafter, err := time.Parse(layout, dateTimeInput)
 	if err != nil {
-		return fmt.Errorf("Erreur lors de l'analyse du temps: %v", err)
+		return fmt.Errorf("Erreur lors de l'analyse du temps (Veuillez entrer une date dans le format 'YYYY-MM-DD' ): %v", err)
+
 	}
 
-	fmt.Print("Entrez le lieu: ")
+	fmt.Print("Entrez la salle que vous souhaiter réserver (1-10): ")
 	fmt.Scanln(&location)
+	if location != "1" && location != "2" && location != "3" && location != "4" && location != "5" && location != "6" && location != "7" && location != "8" && location != "9" && location != "10" {
+		return fmt.Errorf("Salle invalide.")
+	}
 
 	for {
-		fmt.Print("Choisissez une catégorie (Professionnel, Personnel, Loisir): ")
+		fmt.Print("Choisissez la matière enseignée (LangageC, Go, Python): ")
 		fmt.Scanln(&category)
 
-		if category == "Professionnel" || category == "Personnel" || category == "Loisir" {
+		if category == "LangageC" || category == "Go" || category == "Python" {
 			break
 		}
 
@@ -99,7 +127,7 @@ func CreationEvenement(db *sql.DB) error {
 
 	fmt.Println("--------------------------------------------------------------------------------------------------")
 
-	newEvent := Event{
+	newReservation := Reservation{
 		Title:       title,
 		Date:        thenafter.Format("2006-01-02 15:04"),
 		Location:    location,
@@ -107,43 +135,62 @@ func CreationEvenement(db *sql.DB) error {
 		Description: description,
 	}
 
-	err = AddEvent(db, newEvent)
+	err = AddEvent(db, newReservation)
 	if err != nil {
 		return fmt.Errorf("Erreur lors de l'ajout de l'événement: %v", err)
+	}
+
+	// query := "SELECT id FROM reservations WHERE name = ? AND date = ? AND salle_id = ? AND cours = ? AND description = ?"
+	// row := db.QueryRow(query, title, thenafter.Format("2006-01-02 15:04"), location, category, description)
+
+	// var id int
+	// err = row.Scan(&id)
+	// if err != nil {
+	// 	return fmt.Errorf("Erreur lors de la récupération de l'ID de la réservation: %v", err)
+	// }
+
+	locationID, err := strconv.Atoi(location)
+	if err != nil {
+		return fmt.Errorf("Erreur lors de la conversion de la salle en nombre: %v", err)
+	}
+
+	// Pass the integer ID to the function
+	err = MakeSalleIndispobible(db, locationID)
+	if err != nil {
+		return fmt.Errorf("Erreur lors de la mise à jour de la salle: %v", err)
 	}
 
 	fmt.Println("Événement ajouté avec succès !")
 	return nil
 }
 
-func VisualiserEvenements(db *sql.DB) error {
-	fmt.Println("--------------------------------------Visualiser les événements--------------------------------------")
+func VisualiserSalles(db *sql.DB) error {
+	fmt.Println("--------------------------------------Visualiser les Salles--------------------------------------")
 
-	rows, err := db.Query("SELECT * FROM events")
+	rows, err := db.Query("SELECT * FROM salles WHERE dispo = 1")
 	if err != nil {
 		return fmt.Errorf("Erreur lors de la récupération des événements: %v", err)
 	}
 	defer rows.Close()
 
-	fmt.Printf("%-5s | %-20s | %-19s | %-30s | %-15s | %-50s\n", "ID", "Titre", "Date", "Lieu", "Catégorie", "Description")
-	fmt.Println("--------------------------------------------------------------------------------------------------")
+	fmt.Printf("|%-5s | %-20s | %-19s |\n", "numéro", "capacité", "disponibilité")
+	fmt.Println("-----------------------------------------------------")
 
 	for rows.Next() {
-		var event Event
-		if err := rows.Scan(&event.ID, &event.Title, &event.Date, &event.Location, &event.Category, &event.Description); err != nil {
+		var Salle Room
+		if err := rows.Scan(&Salle.ID, &Salle.Name, &Salle.Capacity, &Salle.Dispo); err != nil {
 			return fmt.Errorf("Erreur lors de la lecture de la ligne d'événement: %v", err)
 		}
-
-		fmt.Printf("%-5d | %-20s | %-19s | %-30s | %-15s | %-50s\n", event.ID, event.Title, event.Date, event.Location, event.Category, event.Description)
+		fmt.Printf("|%-5d | %-20d | %-19d |\n", Salle.ID, Salle.Capacity, Salle.Dispo)
 	}
 
-	fmt.Println("--------------------------------------------------------------------------------------------------")
+	fmt.Println("------------------------------------------------------------------------------------------------")
 
 	return nil
 }
 
-func SupprimerEvenement(db *sql.DB, eventID int) error {
-	result, err := db.Exec("DELETE FROM events WHERE id = ?", eventID)
+func SupprimerReservation(db *sql.DB, locationID int) error {
+	result, err := db.Exec("DELETE FROM reservations WHERE id = ?", locationID)
 	if err != nil {
 		return fmt.Errorf("Erreur lors de la suppression de l'événement: %v", err)
 	}
@@ -154,28 +201,99 @@ func SupprimerEvenement(db *sql.DB, eventID int) error {
 	}
 
 	if rowsAffected == 0 {
-		return fmt.Errorf("Aucun événement trouvé avec l'ID %d", eventID)
+		return fmt.Errorf("Aucun événement trouvé avec l'ID %d", locationID)
 	}
 
-	fmt.Printf("Événement avec l'ID %d supprimé avec succès.\n", eventID)
+	fmt.Printf("Événement avec l'ID %d supprimé avec succès.\n", locationID)
 
 	return nil
 }
 
-func AfficherEvenement(db *sql.DB, eventID int) error {
-	fmt.Println("--------------------------------------------------------------------------------------------------")
-	row := db.QueryRow("SELECT * FROM events WHERE id = ?", eventID)
+func VisualiserReservations(db *sql.DB) error {
+	fmt.Println("--------------------------------------Visualiser les réservations--------------------------------------")
 
-	var event Event
-	err := row.Scan(&event.ID, &event.Title, &event.Date, &event.Location, &event.Category, &event.Description)
+	rows, err := db.Query("SELECT name, date, cours, description, salle_id FROM reservations")
 	if err != nil {
-		return fmt.Errorf("Erreur lors de la récupération de l'événement: %v", err)
+		return fmt.Errorf("Erreur lors de la récupération des réservations: %v", err)
+	}
+	defer rows.Close()
+
+	fmt.Printf("|%-20s | %-19s | %-20s | %-19s | %-19s |\n", "name", "date", "cours", "n de salle", "Description")
+	fmt.Println("-------------------------------------------------------------------------------------------------------------------")
+
+	for rows.Next() {
+		var reservation Reservation
+		if err := rows.Scan(&reservation.Title, &reservation.Date, &reservation.Category, &reservation.Description, &reservation.Location); err != nil {
+			return fmt.Errorf("Erreur lors de la lecture de la ligne de réservation: %v", err)
+		}
+		fmt.Printf("|%-20s | %-19s | %-20s | %-19s | %-19s |\n", reservation.Title, reservation.Date, reservation.Category, reservation.Location, reservation.Description)
 	}
 
-	fmt.Printf("ID: %d\nTitre: %s\nDate: %s\nLieu: %s\nCatégorie: %s\nDescription: %s\n",
-		event.ID, event.Title, event.Date, event.Location, event.Category, event.Description)
-
-	fmt.Println("--------------------------------------------------------------------------------------------------")
+	fmt.Println("-------------------------------------------------------------------------------------------------------------------")
 
 	return nil
 }
+
+// func ModifierReservation(db *sql.DB, locationID int) error {
+// 	fmt.Println("--------------------------------------Modifier une réservation--------------------------------------")
+
+// 	var title, date, timeInput, location, category, description string
+
+// 	fmt.Print("Entrez le titre de la réservation: ")
+// 	fmt.Scanln(&title)
+
+// 	fmt.Print("Entrez la date (YYYY-MM-DD): ")
+// 	fmt.Scanln(&date)
+
+// 	fmt.Print("Entrez l'heure (HH:mm): ")
+// 	fmt.Scanln(&timeInput)
+
+// 	// pour éviter l'erreur de temps/date
+// 	dateTimeInput := fmt.Sprintf("%s %s", date, timeInput)
+
+// 	layout := "2006-01-02 15:04"
+// 	thenafter, err := time.Parse(layout, dateTimeInput)
+// 	if err != nil {
+// 		return fmt.Errorf("Erreur lors de l'analyse du temps (Veuillez entrer une date dans le format 'YYYY-MM-DD' ): %v", err)
+
+// 	}
+
+// 	fmt.Print("Entrez la salle que vous souhaiter réserver (1-10): ")
+// 	fmt.Scanln(&location)
+// 	if location != "1" && location != "2" && location != "3" && location != "4" && location != "5" && location != "6" && location != "7" && location != "8" && location != "9" && location != "10" {
+// 		return fmt.Errorf("Salle invalide.")
+// 	}
+
+// 	for {
+// 		fmt.Print("Choisissez la matière enseignée (LangageC, Go, Python): ")
+// 		fmt.Scanln(&category)
+
+// 		if category == "LangageC" || category == "Go" || category == "Python" {
+// 			break
+// 		}
+
+// 		fmt.Println("Choix invalide.")
+// 	}
+
+// 	fmt.Print("Entrez une brève description: ")
+// 	fmt.Scanln(&description)
+
+// 	fmt.Println("--------------------------------------------------------------------------------------------------")
+
+// 	newReservation := Reservation{
+// 		Title:       title,
+// 		Date:        thenafter.Format("2006-01-02 15:04"),
+// 		Location:    location,
+// 		Category:    category,
+// 		Description: description,
+// 	}
+
+// 	err = UpdateEvent(db, newReservation, locationID)
+// 	if err != nil {
+// 		return fmt.Errorf("Erreur lors de la mise à jour de l'événement: %v", err)
+// 	}
+
+// 	locationID, err := strconv.Atoi(location)
+// 	if err != nil {
+// 		return fmt.Errorf("Erreur lors de la conversion de la salle en nombre: %v", err)
+// 	}
--- Projet/main.go
+++ Projet/main.go
@@ -1,10 +1,10 @@
 package main
 
 import (
-	"fmt"
-	"os"
-	"gestionProjetGolang/fonction"
 	"database/sql"
+	"fmt"
+	"gestionProjetGolang/fonction"
+	"os"
 )
 
 var db *sql.DB
@@ -13,11 +13,11 @@ func affichagePrincipal() int {
 	var inputUtilisateur int
 	fmt.Fprintf(os.Stdout, "\033[0;31m%s\033[0m\n", "Bienvenue dans le Système de gestion de plannings") // ligne de bienvenue en rouge
 	fmt.Println("-----------------------------------------------------")
-	fmt.Println("1. Créer un nouvel événement")
-	fmt.Println("2. Visualiser les événements")
-	fmt.Println("3. Modifier un événement")
-	fmt.Println("4. Supprimer un événement")
-	fmt.Println("5. Rechercher un événement")
+	fmt.Println("1. Créer une réservations")
+	fmt.Println("2. Visualiser les salle disponibles")
+	fmt.Println("3. Supprimer une réservation")
+	fmt.Println("4. Visualiser les réservations")
+	fmt.Println("5. Modifier une réservation")
 	fmt.Println("6. Quitter")
 	fmt.Println("Choisissez une option :")
 	fmt.Scanln(&inputUtilisateur)
@@ -27,30 +27,33 @@ func affichagePrincipal() int {
 func choixUtilisateur(choix int) {
 	switch choix {
 	case 1:
-		err := fonction.CreationEvenement(db)
+		err := fonction.CreationReservations(db)
 		if err != nil {
 			fmt.Println("Erreur lors de la création de l'événement:", err)
 		}
 	case 2:
-		fonction.VisualiserEvenements(db)
+		fonction.VisualiserSalles(db)
 	case 3:
-		// modification à faire
+		var LocationID int
+		fmt.Print("Entrez l'ID de l'événement à supprimer: ")
+		fmt.Scanln(&LocationID)
+		err := fonction.SupprimerReservation(db, LocationID)
+		if err != nil {
+			fmt.Println(err)
+		}
 	case 4:
-		var eventID int
-        fmt.Print("Entrez l'ID de l'événement à supprimer: ")
-        fmt.Scanln(&eventID)
-        err := fonction.SupprimerEvenement(db, eventID)
-        if err != nil {
-            fmt.Println(err)
-        }
-	case 5:
-        var eventID int
-        fmt.Print("Entrez le nom de l'événement à afficher: ")
-        fmt.Scanln(&eventID)
-        err := fonction.AfficherEvenement(db, eventID)
-        if err != nil {
-            fmt.Println(err)
-        }
+		err := fonction.VisualiserReservations(db)
+		if err != nil {
+			fmt.Println(err)
+		}
+	// case 5:
+	// 	var eventID int
+	// 	fmt.Print("Entrez le nom de l'événement à afficher: ")
+	// 	fmt.Scanln(&eventID)
+	// 	err := fonction.AfficherEvenement(db, eventID)
+	// 	if err != nil {
+	// 		fmt.Println(err)
+	// 	}
 	case 6:
 		fmt.Println("Merci de votre visite !")
 		os.Exit(0)
